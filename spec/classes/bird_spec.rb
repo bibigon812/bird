01fa4aa0ba4474811907ad96ed8ec7dbc6ffd44b
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-11-17 14:57:48 +0300

    Added tests

--- spec/classes/bird_spec.rb
+++ spec/classes/bird_spec.rb
@@ -8,8 +8,32 @@ describe 'bird' do
       it { is_expected.to compile }
 
       it {
-        is_expected.to contain_service('bird') \
+        is_expected.to contain_service('bird')
           .with_enable(true)
+          .with_hasrestart(true)
+          .with_hasstatus(true)
+          .that_subscribes_to(['Package[bird]', 'Concat[/etc/bird.conf]'])
+      }
+
+      it {
+        is_expected.to contain_package('bird')
+      }
+
+      it {
+        is_expected.to contain_concat('/etc/bird.conf')
+          .that_requires('Package[bird]')
+      }
+
+      it {
+        is_expected.to contain_concat__fragment('bird_conf_10_global')
+          .with_content(<<-EOS
+#
+# Managed by Puppet in the rp_env environment
+#
+
+router id 172.16.254.254
+EOS
+          )
       }
     end
   end
--- spec/defines/bgp_spec.rb
+++ spec/defines/bgp_spec.rb
@@ -15,6 +15,43 @@ describe 'bird::bgp' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_concat__fragment('bird_conf_50_bgp_namevar')
+          .with_content(<<-EOS
+
+protocol bgp namevar {
+    local as 65000
+    neighbor 10.0.0.2
+
+    export none
+    import all
+}
+EOS
+          )
+      }
+
+      context "with remote_as => 65000" do
+        let(:params) {
+          super().merge({ 'remote_as' => 65000 })
+        }
+
+        it {
+          is_expected.to contain_concat__fragment('bird_conf_50_bgp_namevar')
+            .with_content(<<-EOS
+
+protocol bgp namevar {
+    local as 65000
+    neighbor as 65000
+    neighbor 10.0.0.2
+
+    export none
+    import all
+}
+EOS
+            )
+        }
+      end
     end
   end
 end
