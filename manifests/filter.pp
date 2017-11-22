e9c172f2e758ad1f5c3169cc1a4fd3416401891f
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-11-22 07:50:59 +0300

    Defines and variables become a hash.

--- manifests/filter.pp
+++ manifests/filter.pp
@@ -29,29 +29,30 @@
 #
 # @param
 #   body
-#     ...
+#     Contains a filter body. See http://bird.network.cz/?get_doc&f=bird-5.html
 #
 # @param
 #   defines
-#     ...
+#     Contains a hash { constant_name => constant_value, ... }. See
+#     http://bird.network.cz/?get_doc&f=bird-5.html
 #
-# @params
+# @param
 #   variables
-#     ...
+#     Contains a hash { variable_name => variable_type, ... }. See
+#     http://bird.network.cz/?get_doc&f=bird-5.html
 #
 define bird::filter (
   Stdlib::Absolutepath $conf_path,
 
   String[1] $body,
-  Array[Struct[{
-    name  => String[1],
-    value => Variant[Integer, String[1]],
-  }]] $defines = [],
-  Array[Struct[{
-    name => String[1],
-    type => Pattern[/\A(int|pair|quad|ip|prefix|ec|lc|enum)(?:\sset)?\Z/],
-    value => Variant[Integer, String[1]],
-  }]] $variables = [],
+  Hash[
+    String[1],
+    Variant[Integer, String[1]]
+  ] $defines,
+  Hash[
+    String[1],
+    Pattern[/\A(?:int|pair|quad|ip|prefix|ec|lc|enum)(?:\sset)?\Z/]
+  ] $variables,
 ) {
 
   concat::fragment { "bird_conf_20_filter_${name}":
